144a4fe89ec2058b0709a7f29aa13848ce9ffc58
MateoM00 <mateomurataj1@gmail.com>
2023-08-09 01:12:20 +0200

    create models

---
+++ eAuction/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eAuction.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
